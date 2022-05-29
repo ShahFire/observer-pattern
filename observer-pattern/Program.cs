3ce5ce39a8df1b1b09aee25a7f2ae07ecdc1c183
Ches19 <suharik55x55@gmail.com>
2022-05-29 22:36:56 +0300

    Реализовал скелет паттерна

--- observer-pattern/Program.cs
+++ observer-pattern/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary;
 
 namespace observer_pattern
 {
@@ -10,6 +11,30 @@ namespace observer_pattern
     {
         static void Main(string[] args)
         {
+            ConcreteSubject subject = new ConcreteSubject() { State = 0 };
+
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+
+            subject.RegisterObserver(observer1);
+            subject.State = 100;
+
+            Console.WriteLine("Наблюдатель Боб: " + observer1.Counter);
+            Console.WriteLine("Наблюдатель Тед: " + observer2.Counter);
+
+            subject.RegisterObserver(observer2);
+            subject.State = 200;
+
+            Console.WriteLine("Наблюдатель Боб: " + observer1.Counter);
+            Console.WriteLine("Наблюдатель Тед: " + observer2.Counter);
+
+            subject.RemoveObserver(observer2);
+            subject.State = 300;
+
+            Console.WriteLine("Наблюдатель Боб: " + observer1.Counter);
+            Console.WriteLine("Наблюдатель Тед: " + observer2.Counter);
+
+            Console.ReadKey();
         }
     }
 }
